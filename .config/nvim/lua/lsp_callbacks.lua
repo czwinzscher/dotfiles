3168a8cc6c3df7e19aa9dba3168c7d72135c2cdb
clemens <czwinzscher2@gmail.com>
2019-12-21 13:31:33 +0100

    dont check if bufnr is valid

--- .config/nvim/lua/lsp_callbacks.lua
+++ .config/nvim/lua/lsp_callbacks.lua
@@ -1,28 +1,24 @@
 local lsp_callbacks = {}
 
 local util = vim.lsp.util
-local api = vim.api
 
-local lsp_utils = require 'lsp_utils'
+local my_lsp_utils = require 'lsp_utils'
 
 lsp_callbacks['textDocument/publishDiagnostics'] = function(_, _, result)
     if not result then return end
 
     local bufnr = vim.uri_to_bufnr(result.uri)
 
-    if not bufnr then
-        err_message("LSP.publishDiagnostics: Couldn't find buffer for ", result.uri)
-        return
-    end
-
-    lsp_utils.buf_clear_diagnostics(bufnr)
-    lsp_utils.buf_diagnostics_save_positions(bufnr, result.diagnostics)
+    my_lsp_utils.buf_clear_diagnostics(bufnr)
+    my_lsp_utils.buf_diagnostics_save_positions(bufnr, result.diagnostics)
     util.buf_diagnostics_underline(bufnr, result.diagnostics)
-    lsp_utils.show_marks(bufnr, result.diagnostics)
+    my_lsp_utils.buf_show_marks(bufnr, result.diagnostics)
+
     if result.diagnostics then
         for _, v in ipairs(result.diagnostics) do
             v.uri = v.uri or result.uri
         end
+
         util.set_loclist(result.diagnostics)
     end
 end
